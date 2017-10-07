da6aea2424583f316c923061d2cdfb416fbde1ac
lijiaming <lijiaming1984@gmail.com>
2017-10-07 21:38:51 +0800

    add some commnets

--- src/main/java/com/example/registerbean/PersonBeanFactoryPostProcessor.java
+++ src/main/java/com/example/registerbean/PersonBeanFactoryPostProcessor.java
@@ -9,25 +9,34 @@ import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.stereotype.Component;
 
+
+/**
+ * 为了让新注册的bean被AOP支持，必须在BeanFactoryPostProcessor中
+ * 完成新bean的注册
+ */
 @Component
 @Slf4j
 public class PersonBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        log.info("register personManager1>>>>>>>>>>>>>>>>");
         DefaultListableBeanFactory defaultListableBeanFactory
                 = (DefaultListableBeanFactory) beanFactory;
 
+        //注册Bean定义，容器根据定义返回bean
+        log.info("register personManager1>>>>>>>>>>>>>>>>");
         BeanDefinitionBuilder beanDefinitionBuilder =
                 BeanDefinitionBuilder.genericBeanDefinition(PersonManager.class);
-        beanDefinitionBuilder.addPropertyReference("personDao","personDao");
+        beanDefinitionBuilder.addPropertyReference("personDao", "personDao");
         BeanDefinition personManagerBeanDefinition = beanDefinitionBuilder.getRawBeanDefinition();
         defaultListableBeanFactory.registerBeanDefinition("personManager1", personManagerBeanDefinition);
+
+        //注册bean实例
         log.info("register personManager2>>>>>>>>>>>>>>>>");
         PersonDao personDao = beanFactory.getBean(PersonDao.class);
         PersonManager personManager = new PersonManager();
         personManager.setPersonDao(personDao);
-        beanFactory.registerSingleton("personManager2",personManager);
+        beanFactory.registerSingleton("personManager2", personManager);
 
     }
 }
--- src/main/java/com/example/registerbean/PersonManagerRegisterController.java
+++ src/main/java/com/example/registerbean/PersonManagerRegisterController.java
@@ -7,36 +7,69 @@ import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * 使用rest请求的方式动态注册bean和验证
+ *
+ *
+ */
 @RestController
 @Slf4j
 public class PersonManagerRegisterController {
 
+    /**
+     * The Application context.
+     */
     @Autowired
     GenericApplicationContext applicationContext;
 
+    /**
+     * The Bean factory.
+     */
     @Autowired
     ConfigurableBeanFactory beanFactory;
 
+    /**
+     * 动态注册bean，此处注册的bean没有AOP的支持
+     * curl http://localhost:8080/registerPersonManager
+     */
     @GetMapping("/registerPersonManager")
     public void registerPersonManager() {
         PersonDao personDao = applicationContext.getBean(PersonDao.class);
         PersonManager personManager = new PersonManager();
         personManager.setPersonDao(personDao);
         beanFactory.registerSingleton("personManager3", personManager);
+
     }
 
+    /**
+     * 调用bean personManager1，AOP切面有效
+     * curl http://localhost:8080/usePersonManager1
+     * @return the string
+     */
     @GetMapping("/usePersonManager1")
     public String usePersonManager1() {
         PersonManager personManager = applicationContext.getBean("personManager1", PersonManager.class);
         return personManager.newPerson().toString();
     }
 
+    /**
+     * 调用bean personManager2，AOP切面有效
+     * curl http://localhost:8080/usePersonManager2
+     * @return the string
+     */
     @GetMapping("/usePersonManager2")
     public String usePersonManager2() {
         PersonManager personManager = applicationContext.getBean("personManager2", PersonManager.class);
         return personManager.newPerson().toString();
     }
 
+    /**
+     * 调用bean personManager3，AOP切面无效
+     * 请求该方法之前，应该先请求registerPersonManager注册personManager3
+     * curl http://localhost:8080/usePersonManager3
+     * @return the string
+     */
     @GetMapping("/usePersonManager3")
     public String usePersonManager3() {
         PersonManager personManager = applicationContext.getBean("personManager3", PersonManager.class);
