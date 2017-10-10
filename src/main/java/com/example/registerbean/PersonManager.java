37ad7c42b294950a6e999a8b14e6d1bdbe7165db
lijiaming <lijiaming1984@gmail.com>
2017-10-10 21:16:37 +0800

    add unit test

--- src/main/java/com/example/registerbean/PersonManager.java
+++ src/main/java/com/example/registerbean/PersonManager.java
@@ -1,14 +1,17 @@
 package com.example.registerbean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 
-@Service
 @Data
 public class PersonManager {
+
     private PersonDao personDao;
 
     public Person newPerson() {
         return this.personDao.createDemoPerson();
     }
+
 }
--- src/test/java/com/example/registerbean/RegisterbeanApplicationTests.java
+++ src/test/java/com/example/registerbean/RegisterbeanApplicationTests.java
@@ -2,15 +2,30 @@ package com.example.registerbean;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RegisterbeanApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    public void testRegisterBeanInBeanFactoryPostProcessor() {
+        //在PersonBeanFactoryPostProcessor中动态注册了personManager1和personManager2
+        PersonManager personManager1 = applicationContext.getBean("personManager1", PersonManager.class);
+        assertNotNull(personManager1);
+
+        PersonManager personManager2 = applicationContext.getBean("personManager2", PersonManager.class);
+        assertNotNull(personManager2);
+
+        assertFalse(personManager1 == personManager2);
+    }
 
 }
